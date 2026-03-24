9501db98feb472bf1cf8cdab4c598f589aade5fb
Michal Kotecki <michal.kotecki@capgemini.com>
2026-03-24 14:32:22 +0100

    mod_03 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,23 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { BreadcrumbsComponent } from './shared-ui/breadcrumbs/breadcrumbs.component';
 import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -39,8 +56,9 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     MatMenuModule,
     MatButtonModule,
+    RouterModule.forRoot(ROUTES, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
